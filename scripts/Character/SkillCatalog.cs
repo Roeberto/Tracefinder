8f473b9bcf1a09b67ea469b44a23adabc0454de2
roeberto <roboga03@gmail.com>
2026-09-10 22:55:41 +0200

    Dodanie skilli i mapowania ich do AbilityScore

---
+++ scripts/Character/SkillCatalog.cs
@@ -0,0 +1,46 @@
+using System;
+
+
+namespace Tracefinder.Character
+{
+    public class SkillCatalog
+    {
+        public AbilityScore KeyAbilityFor(SkillName skill)
+        {
+            switch (skill)
+            {
+                case SkillName.Athletics:
+                    return AbilityScore.Strength;
+
+                case SkillName.Acrobatics:
+                case SkillName.Stealth:
+                case SkillName.Thievery:
+                    return AbilityScore.Dexterity;
+
+                // case SkillName.
+                //    return AbilityScore.Constitution;
+
+                case SkillName.Arcana:
+                case SkillName.Crafting:
+                case SkillName.Occultism:
+                case SkillName.Society:
+                    return AbilityScore.Intelligence;
+
+                case SkillName.Medicine:
+                case SkillName.Nature:
+                case SkillName.Religion:
+                case SkillName.Survival:
+                    return AbilityScore.Wisdom;
+
+                case SkillName.Deception:
+                case SkillName.Diplomacy:
+                case SkillName.Intimidation:
+                case SkillName.Performance:
+                    return AbilityScore.Charisma;
+                   
+                default:
+                    throw new ArgumentException($"Unknown skill: {skill}");
+            }
+        }
+    }
+}
---
+++ scripts/Character/SkillName.cs
@@ -0,0 +1,26 @@
+using System;
+
+
+namespace Tracefinder.Character
+{
+    public enum SkillName
+    {
+    Acrobatics, 
+    Arcana, 
+    Athletics, 
+    Crafting, 
+    Deception, 
+    Diplomacy, 
+    Intimidation, 
+    Medicine, 
+    Nature, 
+    Occultism, 
+    Performance, 
+    Religion, 
+    Society, 
+    Stealth, 
+    Survival, 
+    Thievery
+    }
+
+}
