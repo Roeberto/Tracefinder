9735e8026ce8d43f63ee4eb87301bbe15ae6df98
roeberto <roboga03@gmail.com>
2026-09-10 23:11:06 +0200

    dodanie class z danymi klasy postaci

---
+++ scripts/Character/ClassStats.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace Tracefinder.Character
+{
+    
+    public class ClassStats
+    {
+        public string Name { get; set; } = "";
+        public List<AbilityScore> KeyAbilityOptions { get; set; } = new();
+        public int HitPointsPerLevel { get; set; }
+        public List<SkillName> TrainedSkills { get; set; } = new();
+        public int BonusSkillCount { get; set; }
+        public ProficiencyRank PerceptionProficiency { get; set; }
+        public ProficiencyRank FortitudeProficiency { get; set; }
+        public ProficiencyRank ReflexProficiency { get; set; }
+        public ProficiencyRank WillProficiency { get; set; }
+        public ProficiencyRank ClassDcProficiency { get; set; }
+        public ProficiencyRank UnarmoredProficiency { get; set; }
+        public List<string> Level1Features { get; set; } = new();
+
+    }
+
+
+
+}
