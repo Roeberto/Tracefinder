9463c081d4e85421fbd411f7b4fb2b1052775b72
Roeberto <roboga03@gmail.com>
2026-09-18 22:34:45 +0200

    dodanie zmiennej Experience do class CharacterCore

--- scripts/Character/CharacterCore.cs
+++ scripts/Character/CharacterCore.cs
@@ -17,7 +17,7 @@ namespace Tracefinder.Character
         public string ClassID { get; set; } = ""; // ID klasy postaci np. wojownik = 0
         public AbilityScore SelectedKeyAbility { get; set; } 
         public List<SkillName> ExtraTrainedSkills = new(); //czy postać ma jeszcze jakieś dodatkowe wytrenowane skille
-
+        public int Experience { get; set; } = 0;
 
         // Połączenie zmiennej postaci z clasą AbilityScore
         public int GetScore(AbilityScore ability)
